f010e0d5ccad5f69283c8381fa23810326042fac
Adrian <adrian.gvazquez@udc.es>
2020-02-08 12:24:13 +0100

    Subir ejercicio 3

--- exercicios/e3_efectos_colaterais/lib/effects.ex
+++ exercicios/e3_efectos_colaterais/lib/effects.ex
@@ -1,2 +1,26 @@
 defmodule Effects do
+
+  def print(n) when n <= 1 do
+    IO.puts(n)
+  end
+
+  def print(n) do
+    print(n-1)
+    IO.puts(n)
+  end
+
+
+  def even_print(n) when n <= 2 do 
+    IO.puts(n)
+  end
+  def even_print(n) when (n > 2 and rem(n,2) == 0) do
+    even_print(n-2)
+    IO.puts(n)
+  end
+
+  def even_print(n) do
+    even_print(n-1)
+  end
+
+
 end
