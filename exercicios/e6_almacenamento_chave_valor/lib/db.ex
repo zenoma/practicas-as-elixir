d4efe1f7ba18db7dac899fac9ec58669433a0ddc
Adrian <adrian.gvazquez@udc.es>
2020-03-02 15:04:31 +0100

    Añadir ejercicio 6

--- exercicios/e6_almacenamento_chave_valor/lib/db.ex
+++ exercicios/e6_almacenamento_chave_valor/lib/db.ex
@@ -1,2 +1,50 @@
 defmodule Db do
+
+def new() do
+    Map.new()
+end
+
+#write(db_ref, key, element)
+def write(map, key, element) do
+    Map.put(map, key, element)
+end
+
+#delete(db_ref, key)
+def delete(map, key) do
+    Map.delete(map, key)
+end
+
+#read(db_ref, key)
+def read(map, key) do
+    case map do
+      %{^key => element} -> {:ok, element}
+
+      _ -> {:error, :not_found}
+    end
+end
+
+#map = %{"a" => 1, "b" => 2, "c" => 3}
+#match(db_ref, element)
+def match(map, element) do
+    filter(map, Map.keys(map), [], element)
+end
+
+
+defp filter(_, [], aux, _) do
+    aux
+end
+
+defp filter(map, [head | tail], aux, element) do
+    if map[head] == element do
+        filter(map, tail, [head] ++ aux, element)
+    else 
+        filter(map, tail, aux, element)
+    end
+end
+
+#destroy(db_ref)
+def destroy(map) do 
+    Map.drop(map, Map.keys(map))
+end
+
 end
