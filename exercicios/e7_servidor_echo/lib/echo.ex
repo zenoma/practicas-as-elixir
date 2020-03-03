6a5c3db968e0d42588b724a1b84bf35ec3840161
Adrian <adrian.gvazquez@udc.es>
2020-03-03 15:58:41 +0100

    Añadir ejercicio 7

--- exercicios/e7_servidor_echo/lib/echo.ex
+++ exercicios/e7_servidor_echo/lib/echo.ex
@@ -1,2 +1,26 @@
 defmodule Echo do
+
+def echo_fun do
+    receive do 
+     msg -> IO.puts(msg)
+    end
+     echo_fun()
+end
+
+def start do
+    Process.register(spawn_link(fn -> echo_fun() end), :echo)
+    :ok
+end
+
+def stop do 
+    Process.exit(Process.whereis(:echo), :normal)
+    Process.unregister(:echo)
+    :ok
+end
+
+def print(term) do
+    #send(:echo, term)
+    Process.send(:echo, term, [])
+end
+
 end
